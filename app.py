fcc4f7b9dae4500bcbbe6b12d18d0018509ee218
Louis-Rene Hache <github@lrhache.com>
2022-12-10 07:52:49 -0500

    ajouter templates

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Bonjour Tristan!!"
+    return render_template("index.html")
+
+
+@app.route("/ma-deuxieme-page")
+def page2():
+    return render_template("page-2.html")
