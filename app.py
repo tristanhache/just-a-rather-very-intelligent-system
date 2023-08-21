98e6337fe47bdf3855116fcda19ec28be58fdbc2
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-08-21 01:13:12 +0000

    ersgtyhrehgrnhgfrs

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+# Store chat messages in a list (for simplicity; you should use a database)
+chat_messages = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', messages=chat_messages)
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    message = request.form.get('message')
+    chat_messages.append(message)
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
