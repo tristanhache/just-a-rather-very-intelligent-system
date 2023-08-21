48a9872a3560881579be5ca3f95958468bbcb788
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-08-21 01:29:36 +0000

    dfdsythfhht

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
 from flask_socketio import SocketIO, join_room, leave_room, emit
 
+import secrets
+
+# Generate a random secret key with 24 bytes (48 characters)
+secret_key = secrets.token_hex(24)
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '\xcbg\xae-\xe4\x95\xb7\x14\xa1\xb5\\ e\x11l\xc5\x13\x0b#\x82\xc8\x18_\xd7'
+app.config['SECRET_KEY'] = secret_key
 socketio = SocketIO(app)
+
+# Dictionary to store chat messages (replace with a database in production)
+chat_messages = {}
 
 @app.route('/')
 def index():
@@ -23,6 +31,9 @@ def handle_join(data):
     join_room(room)
     session['room'] = room
 
+    # Send existing messages when a user joins the room
+    emit('messages', chat_messages.get(room, []))
+
 @socketio.on('leave')
 def handle_leave(data):
     room = session.get('room')
@@ -34,7 +45,16 @@ def handle_leave(data):
 def handle_message(data):
     room = session.get('room')
     if room:
-        emit('message', data, room=room)
+        username = data.get('username', 'Anonymous')
+        message = data['message']
+
+        # Store the message (replace with database storage in production)
+        if room not in chat_messages:
+            chat_messages[room] = []
+        chat_messages[room].append({'username': username, 'message': message})
+
+        # Emit the message to all users in the room
+        emit('message', {'username': username, 'message': message}, room=room)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
