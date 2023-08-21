cd4c015b4de3c8d44eab534a1fb0aa427ef732c3
tristanhache <120027436+tristanhache@users.noreply.github.com>
2023-08-21 00:07:33 +0000

    gfhnkjtfoigbg

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/ma-deuxieme-page")
-def page2():
-    return render_template("page-2.html")  
